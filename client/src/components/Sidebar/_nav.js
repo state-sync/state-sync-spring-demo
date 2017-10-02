3b59d1cdda089dbfa7c9e86d805430dae5d00051
Fedor Ilinykh <ify@kibibox.com>
2017-10-02 16:28:06 +0700

    cleanup

--- client/src/components/Sidebar/_nav.js
+++ client/src/components/Sidebar/_nav.js
@@ -8,55 +8,6 @@ export default {
                 variant: 'info',
                 text: 'NEW'
             }
-        },
-        {
-            title: true,
-            name: 'Schedule',
-            wrapper: {            // optional wrapper object
-                element: '',        // required valid HTML5 element tag
-                attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-            },
-            class: ''             // optional class names space delimited list for title item ex: "text-center"
-        },
-        {
-            name: 'Calendar',
-            url: '/schedule/calendar',
-            icon: 'icon-calendar'
-        },
-        {
-            name: 'Schedules',
-            url: '/schedule/schedules',
-            icon: 'icon-layers'
-        },
-        {
-            name: 'Timetables',
-            url: '/schedule/timetables',
-            icon: 'icon-cursor'
-        },
-        {
-            name: 'Demo',
-            url: '/timetable',
-            icon: 'icon-cursor'
-        },
-        {
-            title: true,
-            name: 'Trucks',
-            wrapper: {            // optional wrapper object
-                element: '',        // required valid HTML5 element tag
-                attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-            },
-            class: ''             // optional class names space delimited list for title item ex: "text-center"
-        },
-        {
-            name: 'Status',
-            url: '/trucks/status',
-            icon: 'icon-speedometer'
-        },
-        {
-            name: 'Manage',
-            url: '/trucks/list',
-            icon: 'icon-cursor'
         }
-
     ]
 };
